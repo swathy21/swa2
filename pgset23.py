65ebd54b32dfd90a9a41dcaa5c84d3e09bc71be2
swathy21 <42170034+swathy21@users.noreply.github.com>
2019-03-07 10:32:13 +0530

    Create pgset23.py

---
+++ pgset23.py
@@ -0,0 +1,7 @@
+number=int(input("enter any number"));
+if number>1:
+      for x in range(2,number):
+        if(number%i)==0:
+        print(number,"is not a prime number")
+        else:
+        print(number,"is a prime number")
